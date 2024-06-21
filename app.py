fefe2ba7ce3745ac5f961e9ee8c09d39408cdde9
wainesville <wainesville@gmail.com>
2024-06-20 21:24:59 -0700

    final SQLALCHEMY part 1

--- app.py
+++ app.py
@@ -10,30 +10,69 @@ app.app_context().push()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'password123'
-app.config['DEBUG_TB_INTERcept_REDIRECTS']=False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS']=False
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+db.create_all()
 
 @app.route('/')
-def home_page():
-    users = User.query.all()
-    return render_template('users.html', users = users)
-@app.route('/new_user.html', methods=['POST'])
+def root():
+    return redirect('/users')
+
+
+@app.route('/users')
+def users_list():
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('users/list.html', users = users)
+
+@app.route('/users/new', methods=["GET"])
+def new_user_form():
+    return render_template('users/new.html')
+
+@app.route('/users/new', methods=['POST'])
 def new_user():
-    first_name = request.form["first_name"]
-    last_name = request.form["last_name"]
-    image_url = request.form["image_url"]
-    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+
+    new_user= User(
+        first_name = request.form["first_name"],
+        last_name = request.form["last_name"],
+        image_url = request.form["image_url"] or None)
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(f"/{new_user.id}")
+    return redirect("/users")
 
-@app.rout('/<int:user_id>')
+@app.route('/users/<int:user_id>')
 def user_details(user_id):
     user = User.query.get_or_404(user_id)
-    return render_template('user_details.html', user=user)
+    return render_template('users/detail.html', user=user)
 
+@app.route('/users/<int:user_id>/edit')
+def users_edit(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('users/edit.html', user=user)
+
+@app.route('/users/<int:user_id>/edit', methods=['POST'])
+def users_update(user_id):
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def users_destroy(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect('/users')
+
+# @app.route('/edit_user')
+# def edit_user()
