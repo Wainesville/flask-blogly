aa870f8de891b07dae6abd031b5381963300ebac
wainesville <wainesville@gmail.com>
2024-06-29 21:27:26 -0700

    Step three flask-blogly

--- models.py
+++ models.py
@@ -1,53 +1,83 @@
-"""Models for Blogly."""
+"""SQLAlchemy models for blogly."""
+
 import datetime
 from flask_sqlalchemy import SQLAlchemy
 
-db=SQLAlchemy()
+db = SQLAlchemy()
 
-DEFAULT_IMAGE_URL = "https://i.sstatic.net/34AD2.jpg"
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
 
-def connect_db(app):
-       db.app = app
-       db.init_app(app)  
 
 class User(db.Model):
-    __tablename__= "users"
+    """Site user."""
 
-    id = db.Column(db.Integer,
-                     primary_key = True)
-    first_name = db.Column(db.Text,
-                           nullable= False)
-    last_name = db.Column(db.Text,
-                          nullable = False)
-    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)  
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
 
     posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
 
     @property
     def full_name(self):
-        return f"{self.first_name} {self.last_name}" 
+        """Return full name of user."""
+
+        return f"{self.first_name} {self.last_name}"
+
 
 class Post(db.Model):
-     __tablename__="posts"
+    """Blog post."""
 
-     id = db.Column(db.Integer,
-                     primary_key = True)
-     title = db.Column(db.Text, 
-                       nullable= False)
-     content = db.Column(db.Text,
-                          nullable = False)
-     created_at = db.Column(db.DateTime,
-                            nullable=False,
-                            default=datetime.datetime.now)
+    __tablename__ = "posts"
 
-     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-     @property
-     def friendly_date(self):
-          return self.created_at.strftime("%a %b %-d %Y,%-I:%M %p")
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
 
-    
-
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
 
 
-            
+class PostTag(db.Model):
+    """Tag on a post."""
+
+    __tablename__ = "posts_tags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+
+class Tag(db.Model):
+    """Tag that can be added to posts."""
+
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    posts = db.relationship(
+        'Post',
+        secondary="posts_tags",
+        # cascade="all,delete",
+        backref="tags",
+    )
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
